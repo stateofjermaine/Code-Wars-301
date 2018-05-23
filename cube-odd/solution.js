cd4366d880a9a0ade9603217b8dacbd4fceae8f9
Jermaine Walker <jermainemwalker@gmail.com>
2018-05-22 22:22:37 -0700

    Completed write-up for kata.

--- cube-odd/solution.js
+++ cube-odd/solution.js
@@ -1,4 +1,10 @@
-function disemvowel( str ) {
-  let newString = str.replace( /[aeiou]/ig, '' );
-  return newString;
+function cubeOdd( arr ) {
+  if ( arr.every( elem => typeof ( elem ) === 'number' ) ) {
+    let filteredArr = arr.filter( elem => elem % 2 === 1 || elem % 2 === -1 );
+    let mappedArr = filteredArr.map( elem => Math.pow( elem, 3 ) );
+    let reducedArr = mappedArr.reduce( ( accum, curr ) => accum + curr, 0 );
+    return reducedArr;
+  } else {
+    return undefined;
+  }
 }
